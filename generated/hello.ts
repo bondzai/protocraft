a95696889144c07e082803ce6d96fb5c45d358ad
JamesBond <introbond.dev@gmail.com>
2023-12-07 09:51:41 +0700

    wip: tidy

--- generated/hello.ts
+++ generated/hello.ts
@@ -3,26 +3,87 @@ import * as _m0 from "protobufjs/minimal";
 
 export const protobufPackage = "";
 
-export interface Hello {
+export interface HelloRequest {
+  name: string;
+}
+
+export interface HelloResponse {
   message: string;
 }
 
-function createBaseHello(): Hello {
+function createBaseHelloRequest(): HelloRequest {
+  return { name: "" };
+}
+
+export const HelloRequest = {
+  encode(message: HelloRequest, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
+    if (message.name !== "") {
+      writer.uint32(10).string(message.name);
+    }
+    return writer;
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): HelloRequest {
+    const reader = input instanceof _m0.Reader ? input : _m0.Reader.create(input);
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = createBaseHelloRequest();
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          if (tag !== 10) {
+            break;
+          }
+
+          message.name = reader.string();
+          continue;
+      }
+      if ((tag & 7) === 4 || tag === 0) {
+        break;
+      }
+      reader.skipType(tag & 7);
+    }
+    return message;
+  },
+
+  fromJSON(object: any): HelloRequest {
+    return { name: isSet(object.name) ? globalThis.String(object.name) : "" };
+  },
+
+  toJSON(message: HelloRequest): unknown {
+    const obj: any = {};
+    if (message.name !== "") {
+      obj.name = message.name;
+    }
+    return obj;
+  },
+
+  create<I extends Exact<DeepPartial<HelloRequest>, I>>(base?: I): HelloRequest {
+    return HelloRequest.fromPartial(base ?? ({} as any));
+  },
+  fromPartial<I extends Exact<DeepPartial<HelloRequest>, I>>(object: I): HelloRequest {
+    const message = createBaseHelloRequest();
+    message.name = object.name ?? "";
+    return message;
+  },
+};
+
+function createBaseHelloResponse(): HelloResponse {
   return { message: "" };
 }
 
-export const Hello = {
-  encode(message: Hello, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
+export const HelloResponse = {
+  encode(message: HelloResponse, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
     if (message.message !== "") {
       writer.uint32(10).string(message.message);
     }
     return writer;
   },
 
-  decode(input: _m0.Reader | Uint8Array, length?: number): Hello {
+  decode(input: _m0.Reader | Uint8Array, length?: number): HelloResponse {
     const reader = input instanceof _m0.Reader ? input : _m0.Reader.create(input);
     let end = length === undefined ? reader.len : reader.pos + length;
-    const message = createBaseHello();
+    const message = createBaseHelloResponse();
     while (reader.pos < end) {
       const tag = reader.uint32();
       switch (tag >>> 3) {
@@ -42,11 +103,11 @@ export const Hello = {
     return message;
   },
 
-  fromJSON(object: any): Hello {
+  fromJSON(object: any): HelloResponse {
     return { message: isSet(object.message) ? globalThis.String(object.message) : "" };
   },
 
-  toJSON(message: Hello): unknown {
+  toJSON(message: HelloResponse): unknown {
     const obj: any = {};
     if (message.message !== "") {
       obj.message = message.message;
@@ -54,15 +115,39 @@ export const Hello = {
     return obj;
   },
 
-  create<I extends Exact<DeepPartial<Hello>, I>>(base?: I): Hello {
-    return Hello.fromPartial(base ?? ({} as any));
+  create<I extends Exact<DeepPartial<HelloResponse>, I>>(base?: I): HelloResponse {
+    return HelloResponse.fromPartial(base ?? ({} as any));
   },
-  fromPartial<I extends Exact<DeepPartial<Hello>, I>>(object: I): Hello {
-    const message = createBaseHello();
+  fromPartial<I extends Exact<DeepPartial<HelloResponse>, I>>(object: I): HelloResponse {
+    const message = createBaseHelloResponse();
     message.message = object.message ?? "";
     return message;
   },
 };
+
+export interface HelloService {
+  SayHello(request: HelloRequest): Promise<HelloResponse>;
+}
+
+export const HelloServiceServiceName = "HelloService";
+export class HelloServiceClientImpl implements HelloService {
+  private readonly rpc: Rpc;
+  private readonly service: string;
+  constructor(rpc: Rpc, opts?: { service?: string }) {
+    this.service = opts?.service || HelloServiceServiceName;
+    this.rpc = rpc;
+    this.SayHello = this.SayHello.bind(this);
+  }
+  SayHello(request: HelloRequest): Promise<HelloResponse> {
+    const data = HelloRequest.encode(request).finish();
+    const promise = this.rpc.request(this.service, "SayHello", data);
+    return promise.then((data) => HelloResponse.decode(_m0.Reader.create(data)));
+  }
+}
+
+interface Rpc {
+  request(service: string, method: string, data: Uint8Array): Promise<Uint8Array>;
+}
 
 type Builtin = Date | Function | Uint8Array | string | number | boolean | undefined;
 
