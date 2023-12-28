e693bd0b79d0087286aeba3385511fc4d68631d0
JamesBond <introbond.dev@gmail.com>
2023-12-28 17:45:00 +0700

    Add echoServer and Server interface

---
+++ server/echoServer.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+	"fmt"
+
+	"github.com/bondzai/protocraft/config"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"gorm.io/gorm"
+)
+
+type echoServer struct {
+	app *echo.Echo
+	db  *gorm.DB
+	cfg *config.Config
+}
+
+func NewEchoServer(cfg *config.Config, db *gorm.DB) Server {
+	return &echoServer{
+		app: echo.New(),
+		db:  db,
+		cfg: cfg,
+	}
+}
+
+func (s *echoServer) Start() {
+	s.app.Use(middleware.Logger())
+
+	serverUrl := fmt.Sprintf(":%d", s.cfg.App.Port)
+	s.app.Logger.Fatal(s.app.Start(serverUrl))
+}
